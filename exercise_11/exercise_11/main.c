970a5313a8927d2671eb7230bfc97a0af1051340
seono9y <144989211+seono9y@users.noreply.github.com>
2023-11-19 00:21:12 +0900

    02

--- exercise_11/exercise_11/main.c
+++ exercise_11/exercise_11/main.c
@@ -8,14 +8,8 @@
 #include <stdio.h>
 
 int main(void) {
-    int i = 10;
-    char c = 'a';
-    int *iptr2;
-    char *cptr2;
-    iptr2 = &i;
-    cptr2 = &c;
-    printf("i : %p\n%p (size:%lu)\n", iptr2, &i, sizeof(iptr2));
-    printf("c : %p\n%p (size:%lu)\n", cptr2, &c, sizeof(cptr2));
-    printf("iptr2 : %p, %i\n", iptr2, *iptr2);
-    return 0;
+    int i = 300; //300으로 변경해서도 해보기
+    int *pi = &i;
+    char *pc = &i;
+    printf("%i, %i, %i\n", i, *pi, *pc);
 }
