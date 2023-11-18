e700f6889c039f11c917ca768ecf4ccab70748ff
seono9y <144989211+seono9y@users.noreply.github.com>
2023-11-19 00:41:36 +0900

    07-2

--- exercise_11/exercise_11/main.c
+++ exercise_11/exercise_11/main.c
@@ -6,15 +6,16 @@
 //
 
 #include <stdio.h>
-char *proverb="All that glisters is not gold.";
-void setPointer(char *q)
-{
-    q = proverb;
+
+char *proverb = "All that glisters is not gold.";
+
+void setPointer(char **q) {
+    *q = proverb;
 }
-int main(void)
-{
+
+int main(void) {
     char *p = "zzz";
-    setPointer(p);
+    setPointer(&p);
     printf("%s \n", p);
     return 0;
 }
